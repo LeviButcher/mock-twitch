8d92a53a52df6bcd28998703dfc346d91bec39c1
LeviButcher <lbutche3@wvup.edu>
2018-05-11 08:58:07 -0400

    hopeful fix for livestreams

--- src/scripts/components/LiveStream.js
+++ src/scripts/components/LiveStream.js
@@ -1,10 +1,10 @@
 import React from 'react';
 
-const playerSource = "http://player.twitch.tv/?channel=";
+const playerSource = "https://player.twitch.tv/?channel=";
 
 export default function LiveStream(props){
 
-    let channelSource = `http://player.twitch.tv/?channel=${props.match.params.channel}`;
+    let channelSource = `https://player.twitch.tv/?channel=${props.match.params.channel}`;
 
     return(
       <div style={{width: '100%', height: '100%'}}>
